6da953573db1f3b2a1961abf2f292c2ad60105e9
Jiawei Luo <weiwei100fun@hotmail.com>
2017-03-19 15:29:09 -0400

    Add cusor and function of switch between multiple console tabs

--- public/src/scripts/BoostConsole.js
+++ public/src/scripts/BoostConsole.js
@@ -1,38 +1,20 @@
 var $ = require('jquery');
 var Console = require('./Console');
 
-var tmlConsole = '<div class="console">' + 
-                    '<div class="container">' +
-                    '</div>' +
-                 '</div>';
-var tmlRow = '<div class="row">' + 
-                '<div class="content"></div>' +
-                '<div class="cursor"></div>' +
-             '</div>';
-
-var createConsoleRow = function (option) {
-    option = option || {};
-    option = Object.assign({}, {
-        editable: false
-    }, option);
-    var el = $(tmlRow);
-    if(option.editable) {
-        el.addClass('editable');
-    }
-    return el;
+var keyFilter = function (keyCode) {
+    var valid =
+        (keyCode > 47 && keyCode < 58)   || // number keys
+        keyCode == 32 || keyCode == 13   || // spacebar & return key(s) (if you want to allow carriage returns)
+        (keyCode > 64 && keyCode < 91)   || // letter keys
+        (keyCode > 95 && keyCode < 112)  || // numpad keys
+        (keyCode > 185 && keyCode < 193) || // ;=,-./` (in order)
+        (keyCode > 218 && keyCode < 223);   // [\]' (in order)
+    return valid;
 };
 
-var createConsoleEl = function () {
-    var newConsole = $(tmlConsole);
-    var newRow = createConsoleRow({editable: true});
-    newConsole.find('.container').append(newRow);
-    return newConsole;
-};
-
-var keyMaps = function (keyCode) {
-    switch(keyCode) {
-        default: return String.fromCharCode(keyCode);
-    }
+var keyEventMap = {
+    8: 'console.char.remove',
+    13: 'console.char.enter'
 };
 
 /**
@@ -40,22 +22,86 @@ var keyMaps = function (keyCode) {
  */
 var BoostConsole = function (element) {
     this.$element = $(element);
+    this.$tab = $(element).find('.tab');
     this.consoles = this.consoles || [];
     this.currentConsole = this.consoles[0];
     if(this.consoles.length === 0) {
-        this.$element.append(createConsoleEl());
-        this.currentConsole = new Console(this.$element.find('.console').first());
-        this.consoles.push(this.currentConsole);
+        this.addConsole();
     }
 
-    $(document).on('keypress', this.input.bind(this));
+    this.$tab.on('click', '.add', this.addButtonHandler.bind(this));
+    this.$tab.on('click', '.tab-console', this.consoleTabHandler.bind(this));
+
+    $(document).on('keydown', this.keyDown.bind(this));
+    $(document).on('keypress', this.keyPress.bind(this));
 };
 
-BoostConsole.prototype.input = function (e) {
-    var keyCode = e.which;
-    var char = keyMaps(keyCode);
-    this.currentConsole.$element.trigger('console.add.chart', char);
+BoostConsole.tmlTabItem = '<li class="tab-item tab-console"></li>';
+
+BoostConsole.prototype.switchConsole = function (id) {
+    var self = this;
+    self.consoles.some(function (cle) {
+        if(cle.id === id) {
+            if(self.currentConsole) {
+                self.currentConsole.hide();
+            }
+            self.currentConsole = cle.show();
+            return true;
+        }
+    });
+    self.$tab.find('.tab-console').removeClass('active').each(function () {
+        if($(this).data('console-id') === id) {
+            $(this).addClass('active');
+        }
+    });
+};
+
+BoostConsole.prototype.addConsole = function () {
+    var newConsoleEl = Console.createConsoleEl().hide();
+    this.$element.append(newConsoleEl);
+    var newConsole = new Console(this.$element.find('.console').last());
+    var newConsoleId = newConsole.id;
+    this.consoles.push(newConsole);
+    var newTab = $(BoostConsole.tmlTabItem)
+        .insertBefore(this.$tab.find('.add'))
+        .text('console - ' + newConsoleId);
+    newTab.data('console-id', newConsoleId);
+    // switch to new console
+    this.switchConsole(newConsole.id);
+};
+
+BoostConsole.prototype.addButtonHandler = function (e) {
+    this.addConsole();
     e.preventDefault();
+    return false;
+};
+
+BoostConsole.prototype.consoleTabHandler = function (e) {
+    var consoleId = $(e.target).data('console-id');
+    if(this.currentConsole.id != consoleId) {
+        this.switchConsole(consoleId);
+    }
+    e.preventDefault();
+    return false;
+};
+
+BoostConsole.prototype.keyDown = function (e) {
+    var keyCode = e.which;
+    var keyEvent = keyEventMap[keyCode];
+    if(keyEvent) {
+        this.currentConsole.$element.trigger(keyEvent);
+        return false;
+    }
+    if(!keyFilter(keyCode)) {
+        return false;
+    }
+};
+
+BoostConsole.prototype.keyPress = function (e) {
+    e.preventDefault();
+    var keyCode = e.which;
+    var char = String.fromCharCode(keyCode);
+    this.currentConsole.$element.trigger('console.chart.add', char);
     return false;
 };
 
--- public/src/scripts/Console.js
+++ public/src/scripts/Console.js
@@ -1,5 +1,12 @@
 var $ = require('jquery');
 
+var getId = (function () {
+    var counter = 1;
+    return function () {
+        return counter++;
+    };
+})();
+
 var defaultConfig = {};
 
 /**
@@ -8,18 +15,87 @@ var defaultConfig = {};
 var Console = function (element, config) {
     config = config || {};
     config = Object.assign({}, defaultConfig, config);
+    this.id = getId();
     this.$element = $(element);
     this.$currentRow = $(element).find('.row:first-child');
 
-    this.$element.on('console.add.chart', this.addChar.bind(this));
+    this.$element.on('console.chart.add', this.addChar.bind(this));
+    this.$element.on('console.char.remove', this.removeChar.bind(this));
+    this.$element.on('console.char.enter', this.enterChar.bind(this));
+};
+
+// class members
+Console.tmlConsole = '<div class="console">' + 
+                       '<div class="container">' +
+                      '</div>' +
+                     '</div>';
+
+Console.basePrefix = '<span>&gt;</span>';
+
+Console.tmlRow = '<div class="row">' + 
+                   '<div class="prefix">' + Console.basePrefix +'</div>' +
+                   '<div class="content"></div>' +
+                   '<div class="cursor"></div>' +
+                 '</div>';
+
+Console.createConsoleRow = function (option) {
+    option = option || {};
+    option = Object.assign({}, {
+        editable: false
+    }, option);
+    var el = $(Console.tmlRow);
+    if(option.editable) {
+        el.addClass('editable');
+    }
+    else {
+        el.find('.cursor').remove();
+    }
+    return el;
+};
+
+Console.createConsoleEl = function () {
+    var newConsole = $(Console.tmlConsole);
+    var newRow = Console.createConsoleRow({editable: true});
+    newConsole.find('.container').append(newRow);
+    return newConsole;
+};
+
+Console.prototype.addRow = function (option) {
+    var newRow = Console.createConsoleRow(option);
+    this.$currentRow.find('.cursor').remove();
+    this.$element.find('.container').append(newRow);
+    this.$currentRow = this.$element.find('.row').last();
 };
 
 Console.prototype.addChar = function (e, char) {
     var span = $('<span></span>');
     span.text(char);
-    //this.$currentRow.find('.content').append(span);
-    span.insertBefore(this.$currentRow.find('.cursor'));
+    this.$currentRow.find('.content').append(span);
     e.preventDefault();
+};
+
+Console.prototype.removeChar = function (e) {
+    this.$currentRow.find('.content span').last().remove();
+    e.preventDefault();
+};
+
+Console.prototype.enterChar = function (e) {
+    var option = {
+        editable: true
+    };
+    this.addRow(option);
+    e.preventDefault();
+    return false;
+};
+
+Console.prototype.show = function () {
+    this.$element.show();
+    return this;
+};
+
+Console.prototype.hide = function () {
+    this.$element.hide();
+    return this;
 };
 
 Console.prototype.constructor = Console;
